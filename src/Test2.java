d4007c105f41d95412b5951c68f569fc7ccbaef2
Nart Khadzhirokov <kh.narttt@gmail.com>
2024-06-16 19:25:55 +0300

    Калькулятор

---
+++ src/Test2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Test2 {
+
+    public static void main (String[] args) {
+        int number;
+        Scanner scanner=new Scanner(System.in);
+        Swi
+
+    }
+}
